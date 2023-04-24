9c7c845cfbb25cc95e016dbca2bbad5ef51bb089
Christian Schmiedt <120606421+cschm1ed@users.noreply.github.com>
2023-04-24 17:29:44 +0200

    fixed include

--- sources/main.c
+++ sources/main.c
@@ -6,11 +6,11 @@
 /*   By: cschmied <cschmied@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 15:38:11 by cschmied          #+#    #+#             */
-/*   Updated: 2023/04/24 15:42:32 by cschmied         ###   ########.fr       */
+/*   Updated: 2023/04/24 17:29:34 by cschmied         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/pipex.h"
+#include "../includes/minishell.h"
 
 int main(int argc, char **argv, char **env)
 {
