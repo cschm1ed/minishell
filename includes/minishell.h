c8380e256162bf6b25168307b65adc3a8b56d50a
Christian Schmiedt <120606421+cschm1ed@users.noreply.github.com>
2023-04-24 17:22:34 +0200

    added incldude, removed workflow

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,17 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cschmied <cschmied@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 15:38:56 by cschmied          #+#    #+#             */
-/*   Updated: 2023/04/24 15:43:06 by cschmied         ###   ########.fr       */
+/*   Updated: 2023/04/24 17:22:08 by cschmied         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+typedef struct s_commands
+{
+    char *raw;
+    char **lexed;
+    char ***parsed;
+}   t_commands;
 
 # include <unistd.h>
 # include <string.h>
@@ -22,3 +29,4 @@
 # include "../libft/libft.h"
 
 #endif
+
