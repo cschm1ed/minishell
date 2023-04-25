f2faae33aa954d9a20fa8c3df6fa78cb3dc78984
Lea Spohle <lspohle@c2s17d107.42wolfsburg.de>
2023-04-25 19:37:10 +0200

    Tmp lexer - not working

--- sources/lexer.c
+++ sources/lexer.c
@@ -6,7 +6,7 @@
 /*   By: lspohle <lspohle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 09:26:15 by lspohle           #+#    #+#             */
-/*   Updated: 2023/04/25 13:18:30 by lspohle          ###   ########.fr       */
+/*   Updated: 2023/04/25 19:36:13 by lspohle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,22 @@
 
 static void	ft_search_for_char(char *s, size_t *i, char c)
 {
+	//printf("Index: %zu\n", *i);
+	while (s[*i] == c)
+		(*i)++;
 	if (c == '"' || c == '\'' || c == '>' || c == '<')
 		(*i)++;
-	while (s[*i] != c && s[*i])
+	while (s[*i] != c && s[*i] && s[*i] != '>' && s[*i] != '<')
 		(*i)++;
 }
+
+// static void	ft_search_for_char(char *s, size_t *i, char c)
+// {
+// 	if (c == '"' || c == '\'')
+// 		(*i)++;
+// 	while (s[*i] != c && s[*i] != '>' || s[*i] != '<' && s[*i])
+// 		(*i)++;
+// }
 
 static int	ft_cnt_sub_cmds(char *s)
 {
@@ -40,20 +51,25 @@ static int	ft_cnt_sub_cmds(char *s)
 	return (cnt);
 }
 
-static void	ft_locate_substr(char *s, size_t *start, size_t *len)
+static int	ft_locate_substr(char *s, size_t *start, size_t *len)
 {
 	while (s[(*start)] == ' ' && s[(*start)])
 		(*start)++;
 	(*len) = (*start);
+	while (s[(*len)] == '>' || s[(*len)] == '<' && ft_isspace(s[(*len + 1)]) == FAILURE)
+	{
+		*len += 1;
+		if ((s[(*len)] != '>' && s[(*len)] != '<'))
+			return (SUCCESS);
+	}
 	if (s[(*len)] == '"' || s[(*len)] == '\'')
 	{
 		ft_search_for_char(s, len, s[(*len)]);
 		(*start)++;
 	}
-	else if (s[(*len)] == '>' && ft_isspace(s[(*len + 1)]) == FAILURE)
-		*len += 1;
 	else
 		ft_search_for_char(s, len, ' ');
+	return (SUCCESS);
 }
 
 static int	ft_check_syntax(char *cmd)
@@ -93,12 +109,14 @@ char	**lexer(char *cmd)
 		return (NULL);
 	sub_cmds = 0;
 	start = 0;
+	//printf("Count: %d\n", ft_cnt_sub_cmds(cmd));
 	while (sub_cmds < ft_cnt_sub_cmds(cmd))
 	{
 		ft_locate_substr(cmd, &start, &len);
 		substr[sub_cmds] = ft_substr(cmd, start, len - start);
 		if (!substr[sub_cmds])
 			return (ft_free_dbl_ptr(substr));
+		//printf("String: %s\n", substr[sub_cmds]);
 		sub_cmds++;
 		start = len;
 	}
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: lspohle <lspohle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 15:38:11 by cschmied          #+#    #+#             */
-/*   Updated: 2023/04/25 13:25:38 by lspohle          ###   ########.fr       */
+/*   Updated: 2023/04/25 18:38:23 by lspohle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int main(int argc, char **argv, char **env)
     {
         commands.raw = ft_readline("minishell: ");
         commands.lexed = lexer(commands.raw);
-        // if (commands.lexed == NULL)
+        if (commands.lexed == NULL)
+            printf("Keep going!\n");
     }
 }
