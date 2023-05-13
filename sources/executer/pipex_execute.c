983ca783a39ce594f29a9910d8aa377ae64ba634
Lea Spohle <lspohle@c2s17d107.42wolfsburg.de>
2023-05-13 18:14:38 +0200

    single commands working

--- sources/executer/pipex_execute.c
+++ sources/executer/pipex_execute.c
@@ -6,7 +6,7 @@
 /*   By: lspohle <lspohle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 21:53:03 by lspohle           #+#    #+#             */
-/*   Updated: 2023/05/13 17:42:10 by lspohle          ###   ########.fr       */
+/*   Updated: 2023/05/13 18:12:43 by lspohle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ int	ft_child_process(t_data *pipex, t_list *parsed, t_info *info)
 	if (parsed->next)
 	{
 		close(pipex->pipe_fd[0]);
-		printf(MAGENTA"IF Executing command: %s\n"ESC, lst_get_parsed(parsed)->cmd);
 		dup2(pipex->pipe_fd[1], STDOUT_FILENO);
 		close(pipex->pipe_fd[1]);
 	}
@@ -33,14 +32,13 @@ int	ft_child_process(t_data *pipex, t_list *parsed, t_info *info)
 	{
 		close(pipex->pipe_fd[0]);
 		close(pipex->pipe_fd[1]);
-		printf(MAGENTA"ELSE Executing command: %s\n"ESC, lst_get_parsed(parsed)->cmd);
 		dup2(pipex->file_fd[1], STDOUT_FILENO);
-		close(pipex->file_fd[1]);
+		if (pipex->file_fd[1] != STDOUT_FILENO)
+			close(pipex->file_fd[1]);
 	}
 	if (parsed != info->commands->parsed)
 	{
 		close(pipex->pipe_fd[1]);
-		printf(RED"IF Executing command: %s\n"ESC, lst_get_parsed(parsed)->cmd);
 		dup2(pipex->pipe_fd[0], STDIN_FILENO);
 		close(pipex->pipe_fd[0]);
 	}
@@ -48,11 +46,10 @@ int	ft_child_process(t_data *pipex, t_list *parsed, t_info *info)
 	{
 		close(pipex->pipe_fd[0]);
 		close(pipex->pipe_fd[1]);
-		printf(RED"ELSE Executing command: %s\n"ESC, lst_get_parsed(parsed)->cmd);
 		dup2(pipex->file_fd[0], STDIN_FILENO);
-		close(pipex->file_fd[0]);
+		if (pipex->file_fd[0] != STDIN_FILENO)
+			close(pipex->file_fd[0]);
 	}
-	// nur bei executable
 	ret = execute_builtin_if(info, parsed);
 	if (ret != 129)
 		exit (ret);
--- sources/executer/pipex_main.c
+++ sources/executer/pipex_main.c
@@ -6,7 +6,7 @@
 /*   By: lspohle <lspohle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 09:21:26 by lspohle           #+#    #+#             */
-/*   Updated: 2023/05/13 17:14:50 by lspohle          ###   ########.fr       */
+/*   Updated: 2023/05/13 17:53:07 by lspohle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int execute(t_info *info, t_list *parsed)
 
 	while (parsed)
 	{
-		printf(GREEN"Executing command: %s\n"ESC, lst_get_parsed(parsed)->cmd);
 		if (pipe(pipex.pipe_fd) == -1)
 			return (info->exit_code = 1, FAILURE);
 		pipex.pid = fork();
@@ -31,32 +30,3 @@ int execute(t_info *info, t_list *parsed)
 	}
 	return (SUCCESS);
 }
-
-// int	pipex(t_info *info, t_list *parsed)
-// {
-// 	t_data		pipex;
-// 	t_parsed  *content;
-// 	int		i;
-
-// 	i = 1;
-// 	while (parsed)
-// 	{
-// 		content = lst_get_parsed(parsed);
-// 		pipex.cmd_path = get_path(lst_get_parsed(parsed)->cmd, info);
-// 		if (!pipex.cmd_path)
-// 			return (FAILURE);
-// 		if (pipe(pipex.pipe_fd) == -1)
-// 			return (info->exit_code = 1, FAILURE);
-// 		pipex.pid = fork();
-// 		if (pipex.pid == -1)
-// 			return (info->exit_code = 1, FAILURE);
-// 		if (pipex.pid == 0)
-// 			ft_child_process(&pipex, parsed, info);
-// 		ft_parent_process(&pipex);
-// 		// ft_free_dbl_ptr(pipex->cmd_split);
-// 		// free(pipex->cmd_path);
-// 		// pipex->cmd_path = NULL;
-		
-// 	}
-// 	return (3);
-// }
