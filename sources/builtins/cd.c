7ece5f04a9a354924b51f172ba71b03ad420ea75
lspohle <121381385+lspohle@users.noreply.github.com>
2023-05-18 18:10:35 +0200

    cd without dir - basics

--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -1,11 +1,14 @@
 #include <minishell.h>
 
 static int go_up_one_dir(t_info *info);
+static int go_to_home_dir(t_info *info);
 
 int	execute_cd(t_info *info, char *dir)
 {
 	char *joined;
 
+	if (!dir)
+		return (go_to_home_dir(info));
 	if (chdir(dir) == 0)
 	{
 		if (!ft_strcmp(dir, ".."))
@@ -35,6 +38,27 @@ static int go_up_one_dir(t_info *info)
 	current_dir = ft_strrchr(info->pwd, '/');
 	len = ft_strlen(info->pwd) - ft_strlen(current_dir);
 	trimmed_pwd = ft_substr(info->pwd, 0, len);
+	if (trimmed_pwd == NULL)
+		return (1);
+	free (info->pwd);
+	info->pwd = trimmed_pwd;
+	if (lst_replace_var_val(info->env_lst, "PWD", trimmed_pwd) == FAILURE)
+		return (1);
+	return (0);
+}
+
+static int go_to_home_dir(t_info *info)
+{
+	char	*trimmed_pwd;
+	int		delimiter;
+	int		i;
+
+	delimiter = 0;
+	i = -1;
+	while (info->pwd[++i] && delimiter <= 2)
+		if (info->pwd[i] == '/')
+			delimiter++;
+	trimmed_pwd = ft_substr(info->pwd, 0, i - 1);
 	if (trimmed_pwd == NULL)
 		return (1);
 	free (info->pwd);
