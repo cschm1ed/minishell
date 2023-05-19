88a22ee3b6b5bdb672dfffbd732323d017ab9d94
lspohle <121381385+lspohle@users.noreply.github.com>
2023-05-19 13:24:02 +0200

    cd fixed - using getcwd

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -31,6 +31,10 @@ char	*ft_strjoin(const char *s1, const char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+	else if (s1 == NULL)
+		return (ft_strdup(s2));
+	else if (s2 == NULL)
+		return (ft_strdup(s1));
 	result = malloc (ft_strlen(s1) + (ft_strlen(s2) + 1) * sizeof(char));
 	if (result == NULL)
 		return (NULL);
--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -1,87 +1,29 @@
 #include <minishell.h>
 
-static int	go_to_home_dir(t_info *info);
-static int	go_up_one_dir(t_info *info);
-static int	go_to_dir(t_info *info, char *dir, int is_joined);
+static int	update_env(t_info *info, char *dir);
 
 int	execute_cd(t_info *info, char *dir)
 {
+	t_variable*	variable;
+
+	variable = (t_variable*)lst_find_node(info->env_lst, "USER")->content;
 	if (!dir)
-		return (go_to_home_dir(info));
+		dir = ft_strjoin("/Users/", variable->value);
 	if (!chdir(dir))
-	{
-		if (!ft_strncmp(dir, "..", 2))
-			return (go_up_one_dir(info));
-		else if (dir[0] == '.')
-			return (0);
-		else if (ft_strchr(dir, '/'))
-			return (go_to_dir(info, dir, TRUE));
-		else
-			return (go_to_dir(info, dir, FALSE));
-	}
-	if (ft_strchr(dir, '/'))
-		return (printf("minishell: %s: %s\n", dir, strerror(errno)), 127);
+		return (update_env(info, dir));
 	return (printf("minishell: cd: %s: %s\n", dir, strerror(errno)), 1);
 }
 
-static int	go_up_one_dir(t_info *info)
+static int	update_env(t_info *info, char *dir)
 {
-	char	*trimmed_pwd;
-	char	*current_dir;
-	size_t	len;
+	char	*updated_pwd;
 
-	current_dir = ft_strrchr(info->pwd, '/');
-	len = ft_strlen(info->pwd) - ft_strlen(current_dir);
-	if (len == 0)
-		len = 1;
-	trimmed_pwd = ft_substr(info->pwd, 0, len);
-	if (!trimmed_pwd)
-		return (1);
+	updated_pwd = getcwd(NULL, 0);
+	if (!updated_pwd)
+		return (perror("minishell: cd"), 1);
 	free (info->pwd);
-	info->pwd = trimmed_pwd;
-	if (lst_replace_var_val(info->env_lst, "PWD", trimmed_pwd) == FAILURE)
-		return (1);
-	return (0);
-}
-
-static int	go_to_dir(t_info *info, char *dir, int is_joined)
-{
-	char	*joined;
-
-	if (is_joined)
-		joined = ft_strdup(dir);
-	else
-		joined = ft_strsjoin(info->pwd, "/", dir);
-	if (!joined)
-		return (perror("malloc"), 1);
-	info->pwd = joined;
-	if (lst_replace_var_val(info->env_lst, "PWD", joined) == FAILURE)
-		return (1);
-	return (0);
-}
-
-static int	go_to_home_dir(t_info *info)
-{
-	t_variable	*variable;
-	char		*trimmed_pwd;
-	int			delimiter;
-	int			i;
-
-	variable = (t_variable*)lst_find_node(info->env_lst, "USER")->content;
-	delimiter = 0;
-	i = -1;
-	while (info->pwd[++i] && delimiter <= 2)
-		if (info->pwd[i] == '/')
-			delimiter++;
-	if (delimiter < 2)
-		trimmed_pwd = ft_strjoin("/Users/", variable->value);
-	else
-		trimmed_pwd = ft_substr(info->pwd, 0, i - 1);
-	if (trimmed_pwd == NULL)
-		return (1);
-	free (info->pwd);
-	info->pwd = trimmed_pwd;
-	if (lst_replace_var_val(info->env_lst, "PWD", trimmed_pwd) == FAILURE)
+	info->pwd = updated_pwd;
+	if (lst_replace_var_val(info->env_lst, "PWD", updated_pwd) == FAILURE)
 		return (1);
 	return (0);
 }
