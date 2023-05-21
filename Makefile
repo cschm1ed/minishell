f12d8ba37020b6220a7d037fd3c57823da79d757
cschmied <cschmied@student.42wolfsburg.de>
2023-05-21 22:48:09 +0200

    some cleanup in executer_execute.c

--- Makefile
+++ Makefile
@@ -39,7 +39,7 @@ OBJS        := $(SRCS:%.c=$(BUILDDIR)/%.o)
 
 # Compiler
 CC          := cc
-CFLAGS      := -g -Wall -Wextra -I ./includes -O2
+CFLAGS      := -g -Wall -Wextra -I ./includes -O2 -D BUFFER_SIZE=25
 LDFLAGS     := -lreadline -g -Wall -Wextra -I ./includes -I $(HOME)/goinfre/.brew/opt/readline/include/ -L $(HOME)/goinfre/.brew/opt/readline/lib/ -lreadline
 MAKE        := make
 RM          := rm -f
--- sources/executer/executer_execute.c
+++ sources/executer/executer_execute.c
@@ -12,23 +12,39 @@
 
 #include <minishell.h>
 
+void handle_files(t_data *pipex, t_list *parsed, t_info *info);
+static void handle_duplications(t_data *pipex, t_list *parsed, t_info *info, int cnt);
+
+
 int	ft_child_process(t_data *pipex, t_list *parsed, t_info *info, int cnt)
 {
 	t_parsed  *content;
 	int		  is_builtin;
 	
 	content = lst_get_parsed(parsed);
-	pipex->file_fd[1] = create_outfiles(parsed);
-	if (pipex->file_fd[1] == -1)
-		execute_exit(info, NULL, 1);
-	pipex->file_fd[0] = check_infiles(parsed);
-	if (pipex->file_fd[0] == -1)
-		execute_exit(info, NULL, 1);
+	handle_files(pipex, parsed, info);
+	handle_duplications(pipex, parsed, info, cnt, content);
+	is_builtin = execute_builtin_if(info, parsed);
+	if (is_builtin != 129)
+		exit (info->exit_code);
+	pipex->cmd_path = get_path(lst_get_parsed(parsed)->cmd, info);
+	if (!pipex->cmd_path)
+		execute_exit(info, NULL, info->exit_code);
+	if (execve(pipex->cmd_path, content->args, info->env) <= -1) 
+		return (info->exit_code = 127, FAILURE);
+	return (SUCCESS);
+}
+
+static void handle_duplications(t_data *pipex, t_list *parsed, t_info *info, int cnt)
+{
+	t_parsed    *content;
+
+	content = lst_get_parsed(parsed);
 	if (parsed->next)
 	{
 		close(pipex->pipe_fd[cnt][0]);
 		if (dup2(pipex->pipe_fd[cnt][1], STDOUT_FILENO) == -1)
-		close(pipex->pipe_fd[cnt][1]);
+			close(pipex->pipe_fd[cnt][1]);
 	}
 	else
 	{
@@ -52,13 +68,14 @@ int	ft_child_process(t_data *pipex, t_list *parsed, t_info *info, int cnt)
 		if (pipex->file_fd[0] != STDIN_FILENO)
 			close(pipex->file_fd[0]);
 	}
-	is_builtin = execute_builtin_if(info, parsed);
-	if (is_builtin != 129)
-		exit (info->exit_code);
-	pipex->cmd_path = get_path(lst_get_parsed(parsed)->cmd, info);
-	if (!pipex->cmd_path)
-		execute_exit(info, NULL, info->exit_code);
-	if (execve(pipex->cmd_path, content->args, info->env) <= -1) 
-		return (info->exit_code = 127, FAILURE);
-	return (SUCCESS);
+}
+
+void handle_files(t_data *pipex, t_list *parsed, t_info *info)
+{
+	pipex->file_fd[1] = create_outfiles(parsed);
+	if (pipex->file_fd[1] == -1)
+		execute_exit(info, NULL, 1);
+	pipex->file_fd[0] = check_infiles(parsed);
+	if (pipex->file_fd[0] == -1)
+		execute_exit(info, NULL, 1);
 }
