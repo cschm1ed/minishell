773d0ffeb659463270123c76ae8cfbbaefcc198b
Lea Spohle <lspohle@c2s17d107.42wolfsburg.de>
2023-05-22 18:30:00 +0200

    cd - fixed error msg (invalid dir)

--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -1,16 +1,21 @@
 #include <minishell.h>
+#include <dirent.h>
 
 static int	update_env(t_info *info);
 
 int	execute_cd(t_info *info, char *dir)
 {
 	t_variable*	variable;
+	DIR*		directory;
 
 	variable = (t_variable*)lst_find_node(info->env_lst, "USER")->content;
 	if (!dir)
 		dir = ft_strjoin("/Users/", variable->value);
 	if (!chdir(dir))
 		return (update_env(info));
+	directory = opendir(dir);
+	if (directory)
+		closedir(directory);
 	return (ft_printf("minishell: cd: %s: %s\n", dir, strerror(errno)), 1);
 }
 
