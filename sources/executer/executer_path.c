3f43981f6eeead59f1e2da76bcf1e959d9943b0f
Lea Spohle <lspohle@c2s17d107.42wolfsburg.de>
2023-05-29 15:27:42 +0200

    executer - norminette fixed

--- sources/executer/executer_path.c
+++ sources/executer/executer_path.c
@@ -6,13 +6,14 @@
 /*   By: lspohle <lspohle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 17:06:49 by cschmied          #+#    #+#             */
-/*   Updated: 2023/05/24 18:33:50 by lspohle          ###   ########.fr       */
+/*   Updated: 2023/05/29 15:26:17 by lspohle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-static char	*get_relative_path(char *cmd, t_info *info);
+static char	*get_possible_paths(char *cmd, t_info *info);
+static char	*get_relative_path(char *cmd, t_info *info, char **paths);
 
 char	*get_path(char *cmd, t_info *info)
 {
@@ -25,19 +26,17 @@ char	*get_path(char *cmd, t_info *info)
 				ft_printf("minishell: %s: command not found\n", cmd), NULL);
 		return (ft_strdup(cmd));
 	}
-	path = get_relative_path(cmd, info);
+	path = get_possible_paths(cmd, info);
 	if (path == NULL)
 		return (g_exit_code = 127,
 			ft_printf("minishell: %s: command not found\n", cmd), NULL);
 	return (path);
 }
 
-static char	*get_relative_path(char *cmd, t_info *info)
+static char	*get_possible_paths(char *cmd, t_info *info)
 {
-	char	*joined;
 	char	**paths;
 	char	*path_var;
-	int		i;
 
 	path_var = lst_find_var_val(info->env_lst, "PATH");
 	if (path_var == NULL)
@@ -46,6 +45,14 @@ static char	*get_relative_path(char *cmd, t_info *info)
 	paths = ft_split(path_var, ':');
 	if (paths == NULL)
 		exit_error(info, __FILE__, __LINE__, "malloc");
+	return (get_relative_path(cmd, info, paths));
+}
+
+static char	*get_relative_path(char *cmd, t_info *info, char **paths)
+{
+	char	*joined;
+	int		i;
+
 	i = -1;
 	while (paths[++i])
 	{
